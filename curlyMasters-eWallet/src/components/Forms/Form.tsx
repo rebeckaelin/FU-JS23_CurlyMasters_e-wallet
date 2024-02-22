2ae9520c1610e9cd5080121b6cddca98e22e9058
rebeckaelin <rebeckaelin@hotmail.com>
2024-02-22 15:51:41 +0100

    added typescript to form

--- curlyMasters-eWallet/src/components/Forms/Form.tsx
+++ curlyMasters-eWallet/src/components/Forms/Form.tsx
@@ -1,19 +1,24 @@
-<<<<<<< HEAD
-import {useState} from "react";
-
+import React, {useState, ChangeEvent, FormEvent} from "react";
 import {cardList} from "../../constants/CardList";
 import {useNavigate} from "react-router-dom";
-
 import "./Form.scss";
-import Bitcoin from "./../../assets/Bitcoin.svg";
-import EvilCorp from "./../../assets/Evilcorp.svg";
-import Blockchain from "./../../assets/Blockchain.svg";
-import Ninjabank from "./../../assets/Ninjabank.svg";
+import Bitcoin from "../../assets/Bitcoin.svg";
+import EvilCorp from "../../assets/Evilcorp.svg";
+import Blockchain from "../../assets/Blockchain.svg";
+import Ninjabank from "../../assets/Ninjabank.svg";
 
-const Form = () => {
+interface FormData {
+  cardnumber: string;
+  name: string;
+  validthru: string;
+  ccv: string;
+  vendor: string;
+}
+
+const Form: React.FC = () => {
   const navigate = useNavigate();
 
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormData>({
     cardnumber: "",
     name: "",
     validthru: "",
@@ -21,11 +26,13 @@ const Form = () => {
     vendor: "",
   });
 
-  const handleChange = (e) => {
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ): void => {
     setFormData({...formData, [e.target.name]: e.target.value});
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
 
     let backgroundColor = "";
@@ -58,7 +65,7 @@ const Form = () => {
       selectedIcon: selectedIcon,
       state: false,
     });
-    // console.log("cardList", cardList);
+
     navigate("/");
   };
 
@@ -70,8 +77,8 @@ const Form = () => {
           <input
             onChange={handleChange}
             pattern="[0-9\s]{13,19}"
-            minLength="16"
-            maxLength="16"
+            minLength={16}
+            maxLength={16}
             type="text"
             name="cardnumber"
             inputMode="numeric"
@@ -92,8 +99,8 @@ const Form = () => {
           <label htmlFor="validthru">VALID THRU</label>
           <input
             onChange={handleChange}
-            minLength="4"
-            maxLength="4"
+            minLength={4}
+            maxLength={4}
             type="text"
             name="validthru"
             value={formData.validthru}
@@ -103,7 +110,7 @@ const Form = () => {
           <label htmlFor="ccv">CCV</label>
           <input
             onChange={handleChange}
-            maxLength="3"
+            maxLength={3}
             type="text"
             name="ccv"
             value={formData.ccv}
@@ -132,134 +139,3 @@ const Form = () => {
 };
 
 export default Form;
-
-// const [cardList, setCardList] = useState([]);
-
-// const handleSubmit = (e) => {
-//   e.preventDefault();
-//   // console.log("formData in handleSubmit", formData);
-//   // console.log("cardList at start of handleSubmit", cardList);
-
-//   const newCard = {
-//     ...formData,
-//     id: cardList.length + 1,
-//   };
-
-//   const newCardList = [...cardList, newCard];
-//   // console.log("newCardList", newCardList);
-//   setCardList(newCardList);
-
-//   console.log("cardList", cardList);
-// };
-=======
-import { useState, ChangeEvent, FormEvent } from 'react';
-import './Form.scss';
-/* import Button from './Button */
-
-interface FormProps {
-    onSubmit: (cardInfo: CardInfo) => void; // Definiera en passande typ för onSubmit-callbacken
-}
-
-interface CardInfo {
-    cardNumber: string;
-    cardName: string;
-    monthExpire: string;
-    yearExpire: string;
-    ccv: string;
-    vendor: string;
-}
-
-
-function Form ({ onSubmit }: FormProps) {
-
-    // Samlar alla separata state-variabler i en cardInfo-state variabel. Blir lättare att uppdatera hela fältet då. 
-    const [cardInfo, setCardInfo] = useState<CardInfo>({
-        cardNumber: '',
-        cardName: '',
-        monthExpire: '',
-        yearExpire: '',
-        ccv: '',
-        vendor: ''
-    });
-    
-
-    // Hanterar om det blir någon som helst förändring i formuläret. E representerar det event som triggats när vi tryckt/gjort något på formuläret. 
-    const handleChangeInForm = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const { name, value } = e.target; // array destructuring. dvs. vi hämtar här värdet av e.name och e.value. 
-        setCardInfo({ // här uppdateras sedan det vi extraherat från name och value, och läggs in i cardInfo. Spridningsoperatorn är där för att kopiera och inte ta bort det redan befintliga innehållet
-            ...cardInfo,
-            [name]: value, // i setCardInfo så ska då slutligen namnet och värdet läggas in. 
-        });
-    };
-
-
-
-    // Hanterar när vi tryckt på "submit", eller i vårt fall så blir det ju "add card"
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        onSubmit(cardInfo); // Här ska alltså innehållet i cardInfo att presenteras med onSubmit. 
-
-        // Återställer cardInfo till ursprungliga tomma värden efter att formuläret skickats
-        setCardInfo({
-            cardNumber: '',
-            cardName: '',
-            monthExpire: '',
-            yearExpire: '',
-            ccv: '',
-            vendor: '',
-        });
-    };
-
-
-    // och nedan följer den roliga JSXEN. :D yaaaaaaaaay 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-
-                <input
-                type="text"
-                name="cardNumber"
-                value={cardInfo.cardNumber}
-                onChange={handleChangeInForm}
-                placeholder="Card Number"
-                />
-
-                <input
-                type="text"
-                name="cardName"
-                value={cardInfo.cardName}
-                onChange={handleChangeInForm}
-                placeholder="Card Name"
-                />
-
-                <input
-                type="text"
-                name="monthExpire"
-                value={cardInfo.monthExpire}
-                onChange={handleChangeInForm}
-                placeholder="Month Expire"
-                />
-
-                <input
-                type="text"
-                name="ccv"
-                value={cardInfo.ccv}
-                onChange={handleChangeInForm}
-                placeholder="CCV"
-                />
-
-                <select name="vendor" value={cardInfo.vendor} onChange={handleChangeInForm}>
-                    <option value="">Choose a bank</option>
-                    <option value="Bitcoin INC">Bitcoin INC</option>
-                    <option value="Ninja Bank">Ninja Bank</option>
-                    <option value="Block Chain INC">Block Chain INC</option>
-                    <option value="Evil Corp">Evil Corp</option>
-                </select>
-
-            </form>
-        </div>
-    );
-}
-
-export default Form;
->>>>>>> 825116492597a12aaa87b177a4298de0756f2524
